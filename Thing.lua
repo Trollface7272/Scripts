48f7604a249d546c64470b1864aaa1238aa4d6e0
Trollface7272 <49081230+Trollface7272@users.noreply.github.com>
2021-05-13 20:56:27 +0200

    Update Thing.lua

--- Thing.lua
+++ Thing.lua
@@ -24,8 +24,10 @@ local Images = {
         end
     }
 }
+
 Images.onMenu:Add("https://i.ibb.co/VM0Gj36/IMGBIN-asuka-langley-soryu-kaworu-nagisa-evangelion-chronicle-rebuild-of-evangelion-png-v-BD0m-Bb5.png", "Asuka")
 Images.onMenu:Add("https://i.ibb.co/Q8Nk4xm/5e81cd15b4c94.png", "Rem")
+Images.onMenu:Add("http://trollface.dk/trollfaceONE.png", "Trollface")
 
 Images.background:Add("https://i.ibb.co/K6nyrp8/pngguru-com-1.png", "Neko1")
 Images.background:Add("https://i.ibb.co/wWXDk0d/pngguru-com-2.png", "Neko2")
@@ -37,6 +39,8 @@ Images.background:Add("https://i.ibb.co/1TX80n0/5e81d78ee27f4.png", "Asuna")
 Images.background:Add("https://i.ibb.co/KrSg1Yq/5e8389815d2b9.png", "Nami")
 Images.background:Add("https://i.ibb.co/bggnMQG/5e8391005246e.png", "Rias")
 Images.background:Add("https://i.ibb.co/2cVr8wB/5e8391dddd943.png", "Koneko")
+Images.background:Add("http://trollface.dk/trollfaceONE.png", "Trollface")
+
 
 local w, h = draw.GetScreenSize()
 
@@ -63,13 +67,55 @@ local Menu = {
         url    = gui.Editbox(backgroundBox, "BGIMAGE.CUSTOM.URL", "Custom Image URL"),
     }
 }
+
+local function Split(inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+local function AddToCfg(mode, name, link)
+    local a = file.Open("Weebware.dat", "a")
+    a:Write(mode .. "," .. name .. "," .. link .. "\n")
+    a:Close()
+end
+
+local function LoadConfig()
+    local exists = false
+    file.Enumerate(function(name)
+        if name == "Weebware.dat" then exists = true end
+    end)
+    local a
+    if not exists then a = file.Open("Weebware.dat", "w") a:Write("") a:Close() end
+    a = file.Open("Weebware.dat", "r"):Read()
+    a = Split(a, "\n")
+    for k,v in pairs(a) do
+        local data = Split(v, ",")
+        if data[1] == "background" then 
+            Images.background:Add(data[3], data[2])
+        else
+            Images.onMenu:Add(data[3], data[2])
+        end
+    end
+    Menu.background.image:SetOptions("Off", unpack(Images.background.names))
+    Menu.onMenu.image:SetOptions("Off", unpack(Images.onMenu.names))
+end
+LoadConfig()
+
 gui.Button(onMenuBox, "Add", function()
     Images.onMenu:Add(Menu.onMenu.url:GetValue(), Menu.onMenu.name:GetValue())
     Menu.onMenu.image:SetOptions("Off", unpack(Images.onMenu.names))
+    AddToCfg("onMenu", Menu.onMenu.name:GetValue(), Menu.onMenu.url:GetValue())
 end)
 gui.Button(backgroundBox, "Add", function()
     Images.background:Add(Menu.background.url:GetValue(), Menu.background.name:GetValue())
     Menu.background.image:SetOptions("Off", unpack(Images.background.names))
+    AddToCfg("background", Menu.background.name:GetValue(), Menu.background.url:GetValue())
 end)
 
 local function OnMenu()
